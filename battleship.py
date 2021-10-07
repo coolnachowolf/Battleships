8bb712152bf0534d7f238f9e80b9543965d0dee4
coolnachowolf <89766869+coolnachowolf@users.noreply.github.com>
2021-10-07 15:54:25 +0530

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -381,5 +381,5 @@ def runSimulation(w, h):
 # This code runs the test cases to check your work
 if __name__ == "__main__":
     ## Finally, run the simulation to test it manually ##
-    # runSimulation(500, 500)
+    runSimulation(500, 500)
 
